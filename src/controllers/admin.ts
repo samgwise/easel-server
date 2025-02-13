b3152b7026b8783a299ee8dc2c26226cd104b2c8
Samuel Gillespie <samuel.gillespie@sydney.edu.au>
2025-02-13 16:19:13 +1100

    filled out more of the layout
    Stubbed first model file

---
+++ src/controllers/admin.ts
@@ -0,0 +1,9 @@
+import type {MojoContext} from '@mojojs/core';
+
+export default class Controller {
+  // UoS paths
+  async listUnitsOfStudy (ctx: MojoContext): Promise<void> {
+    ctx.stash.uos = ctx.models.
+    await ctx.render();
+  }
+}
